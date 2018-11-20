84a44ac1b349ce5e984113f19c89a231c16a5d1b
Frederico Vaz <fredericovaz@MacBook-Pro-de-Frederico.local>
2018-11-20 18:01:59 +0000

    update

--- tank_system.m
+++ tank_system.m
@@ -1,29 +1,32 @@
 
 %% The dynamic equations of the liquid levels system:
 
-function [ ht1 ht2 v2 ] = tank_system(v, h1, h2, f1, f2)
+function [ Ht1, Ht2 ] = tank_system(Vp, H1, H2) 
 
-v2 = v2 + v;
+% f1 e f2, depois colocar pertubações no liquido como entrada
 
-% the minimum applied voltage
-if v2 < 0
-    v2 = 0;
-% the maximum applied voltage     
-elseif v2 > Vmax
-    v2 = Vmax;
-end
+global A1 A2 a1 a2 g p km 
 
-v1 = 0.7*v2;
 
 % Liquid level tank 1
-H1 = (-a1/A1) * sqrt(2*g*H1) + () * Vp;
-
-%ht1 = (-a1*sqrt(2*g*h1)-sgn(h1-h2)*a12*sqrt(2*g*abs(h1-h2))+K1*v1+f1)/A1;
+Ht1 = (-a1/A1) * sqrt(2*g*H1) + ((p*km)/A1) * Vp;
 
 % Liquid level tank 2
-%ht2 = (-a2*sqrt(2*g*h2)+sgn(h1-h2)*a12*sqrt(2*g*abs(h1-h2))+K2*v2+f2)/A2;
-
+Ht2 = (-a2/A2) * sqrt(2*g*H2) + (a1/A2)*sqrt(2*g*H1) + (((1-p)*km)/A2) * Vp;
 
 
 end 
+
+% v2 = v2 + v;
+% 
+% % the minimum applied voltage
+% if v2 < 0
+%     v2 = 0;
+% % the maximum applied voltage     
+% elseif v2 > Vmax
+%     v2 = Vmax;
+% end
+% 
+% v1 = 0.7*v2;
+% % % ^ver melhor esta parte!
 
